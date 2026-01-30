74280f3add543e61832aa5ee24bc492a3b018664
Ievgen German <german.kbrza@gmail.com>
2026-01-30 23:31:04 +0200

    solution

--- db/models.py
+++ db/models.py
@@ -1,5 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models import DateTimeField, ForeignKey, IntegerField
 
+from django.core.exceptions import ValidationError
 
 class Genre(models.Model):
     name = models.CharField(max_length=255, unique=True)
@@ -17,7 +20,7 @@ class Actor(models.Model):
 
 
 class Movie(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, db_index=True)
     description = models.TextField()
     actors = models.ManyToManyField(to=Actor, related_name="movies")
     genres = models.ManyToManyField(to=Genre, related_name="movies")
@@ -50,3 +53,42 @@ class MovieSession(models.Model):
 
     def __str__(self) -> str:
         return f"{self.movie.title} {str(self.show_time)}"
+
+class User(AbstractUser):
+    pass
+
+
+class Order(models.Model):
+    created_at = DateTimeField(auto_now_add=True)
+    user = ForeignKey(to=User, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.created_at.strftime("%Y-%m-%d %H:%M:%S")
+
+
+class Ticket(models.Model):
+    movie_session = ForeignKey(to=MovieSession, on_delete=models.CASCADE)
+    order = ForeignKey(to=Order, on_delete=models.CASCADE)
+    row = IntegerField()
+    seat = IntegerField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['row', 'seat', 'movie_session'],
+                name='unique_ticket_booking'
+            )
+        ]
+
+    def __str__(self) -> str:
+        return f"<Ticket: {self.movie_session.movie.title} {self.order} (row: {self.row}, seat: {self.seat})>"
+
+    def clean(self) -> None:
+        if not (1 <= self.seat <= self.movie_session.cinema_hall.seats_in_row):
+            raise ValidationError({
+                "seat": f"Seat number must be in range [1, {self.movie_session.cinema_hall.seats_in_row}]."
+            })
+        if not (1 <= self.row <= self.movie_session.cinema_hall.rows):
+            raise ValidationError({
+                "row": f"Row number must be in range [1, {self.movie_session.cinema_hall.rows}]."
+            })
